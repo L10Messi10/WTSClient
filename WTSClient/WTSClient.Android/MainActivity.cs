b54435ab5830180febbd18e016f52f7ecdb94dd1
L10Messi10 <loveyourenemy@outlook.com>
2021-03-13 13:04:51 +0800

    fixes and improvements

--- WTSClient/WTSClient.Android/MainActivity.cs
+++ WTSClient/WTSClient.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace WTSClient.Droid
 {
@@ -18,7 +19,7 @@ namespace WTSClient.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
